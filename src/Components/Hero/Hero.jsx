e71978b000884e2659ae7cfefdaed502b5bda2aa
DurgeshSingh143 <durgeshsinghsolanki181@gmail.com>
2025-03-28 10:46:20 +0530

    portfolio files upload

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Hero.css'
+// import profile_img from '../../assets/profile_img.svg'
+import profile_img from '../../assets/DSS.jpg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Hero = () => {
+  return (
+    <div id='home' className='hero'>
+      <img src={profile_img} alt="" className='profile-img' />
+      <h1><span>I'm Durgesh Singh,</span> frontend developer based in INDIA.</h1>
+      <p>I am a frontend developer from Rajasthan, INDIA with 6 months of experience.</p>
+      <div className="hero-action">
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/assets/services_data.js
@@ -0,0 +1,40 @@
+const Services_Data = [
+  {
+    s_no: "01",
+    s_name: "HTML",
+    s_desc:
+      "Structure, Elements, Tags, Attributes, Forms, Semantic, Links, Tables, Headings, Lists....",
+  },
+  {
+    s_no: "02",
+    s_name: "CSS",
+    s_desc:
+      "Styling, Selectors, Flexbox, Grid, Colors, Animations, Fonts, Media Queries, Transitions, Borders...",
+  },
+  {
+    s_no: "03",
+    s_name: "JavaScript",
+    s_desc:
+      "Variables, Functions, Objects, Arrays, Events, Promises, Loops, DOM, Async, ES6...",
+  },
+  {
+    s_no: "04",
+    s_name: "React",
+    s_desc:
+      "Components, Props, State, Hooks, JSX, Virtual DOM, Context API, Router, Redux, Effects...",
+  },
+  {
+    s_no: "05",
+    s_name: "Git and GitHub",
+    s_desc:
+      "Version Control, Repository, Commit, Branch, Merge, Push, Pull, Fork, Issues, Collaboration...",
+  },
+  {
+    s_no: "06",
+    s_name: "Tailwind CSS",
+    s_desc:
+      "Utility-First, Flex, Grid, Responsive, Components, Shadows, Colors, Padding, Margin, Typography...",
+  },
+];
+
+export default Services_Data;
