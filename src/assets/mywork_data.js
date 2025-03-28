6fae84251862ffb540213bb775212778968bb3c5
DurgeshSingh143 <durgeshsinghsolanki181@gmail.com>
2025-03-28 15:53:02 +0530

    updated mywork_data file

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,41 +1,59 @@
-import project1_img from '../assets/netflix.png'
-import project2_img from '../assets/youtube.png'
-import project3_img from '../assets/gpt-3.png'
-import project4_img from '../assets/hoobank.png'
-import project5_img from '../assets/resturant-app.png'
-import project6_img from '../assets/quiz-app.png'
+import project1_img from "../assets/netflix.png";
+import project2_img from "../assets/youtube.png";
+import project3_img from "../assets/gpt-3.png";
+import project4_img from "../assets/hoobank.png";
+import project5_img from "../assets/resturant-app.png";
+import project6_img from "../assets/quiz-app.png";
+import project7_img from "../assets/tic-tac-toe.png";
+import project8_img from "../assets/todo-app.png";
+import project9_img from "../assets/weather-app.png";
 
 const mywork_data = [
-    {
-        w_no:1,
-        w_name:"Web design",
-        w_img:project1_img
-    },
-    {
-        w_no:2,
-        w_name:"Web design",
-        w_img:project2_img
-    },
-    {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project3_img
-    },
-    {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project4_img
-    },
-    {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
-    },
-    {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
-    },
-]
- 
+  {
+    w_no: 1,
+    w_name: "Web design",
+    w_img: project1_img,
+  },
+  {
+    w_no: 2,
+    w_name: "Web design",
+    w_img: project2_img,
+  },
+  {
+    w_no: 3,
+    w_name: "Web design",
+    w_img: project3_img,
+  },
+  {
+    w_no: 4,
+    w_name: "Web design",
+    w_img: project4_img,
+  },
+  {
+    w_no: 5,
+    w_name: "Web design",
+    w_img: project5_img,
+  },
+  {
+    w_no: 6,
+    w_name: "Web design",
+    w_img: project6_img,
+  },
+  {
+    w_no: 7,
+    w_name: "Web design",
+    w_img: project7_img,
+  },
+  {
+    w_no: 8,
+    w_name: "Web design",
+    w_img: project8_img,
+  },
+  {
+    w_no: 9,
+    w_name: "Web design",
+    w_img: project9_img,
+  },
+];
+
 export default mywork_data;
